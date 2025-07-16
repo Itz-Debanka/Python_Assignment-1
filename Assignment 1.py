a88efd1284c6f7f44989564197763699664a73e3
Debanka <debanka.official@gmail.com>
2025-07-17 01:18:17 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,15 @@
+
+#Task 1
+a=input("Enter bigger number:   ")
+a=int(a)
+b=input("Enter smaller number:   ")
+b=int(b)
+sum=a+b
+subs=a-b
+multiplication=a*b
+division=a/b
+print("sum= ",sum)
+print("substraction= ",subs)
+print("multiplication= ",multiplication)
+print("division=  ",division)
+
---
+++ assignment 1 task 2.py
@@ -0,0 +1,7 @@
+#Task 2
+f=input("enter your first name: ")
+l=input("enter your last name: ")
+f=str(f)
+l=str(l)
+n=f+" "+ l
+print(" Hello,",n,"! welcome to the Python program.")
